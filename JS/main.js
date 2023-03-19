4300d12bb2b831103d6426e42c86c8a6bd1f04de
carrottown <anabianchini@outlook.com.br>
2023-03-19 18:15:13 -0300

    add  movement in prices

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,24 @@
-var annually = document.querySelectorAll(".annually")
-var check = document.querySelector(".toggle-btn label")
+let container_basic = window.document.getElementById('container_basic')
+let container_professional = window.document.getElementById('container_professional')
+let container_master = window.document.getElementById('container_master')
 
-check.addEventListener("click", function() {
-    if(annually.style.display === "none") {
-        annually.style.display = "block"  
-    } else {
-        annually.style.display = "none";
-    }
-})
+let contador = 0
+
+function clicado() {
+    if(contador === 0){
+        console.log("desativado")
+        
+        container_basic.innerHTML = '<h2>&dollar;199.99</h2>'
+        container_professional.innerHTML = '<h2>&dollar;249.99</h2>'
+        container_master.innerHTML = '<h2>&dollar;399.99</h2>'
+
+        contador = contador + 1
+    }else {
+        
+        container_basic.innerHTML = '<h2>&dollar;19.99</h2>'
+        container_professional.innerHTML = '<h2>&dollar;24.99</h2>'
+        container_master.innerHTML = '<h2>&dollar;39.99</h2>'
+
+        contador = contador - 1
+    }  
+}
